7f770fbeef0a3c922009843d568f9c1f13917944
Savannah Ingle <69727315+sningle@users.noreply.github.com>
2022-02-24 16:26:50 -0500

    Removing extraneous comments

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./coin.mjs"
 import minimist from "minimist";
 import express from "express"
-// dconst express = require('express')
 const app = express()
 const args = minimist(process.argv.slice(2))
 args["port"]
 const port = args.port || 5000
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200
-//     res.setHeader('Content-Type', 'text/html')
-//     res.end(data)
-// })
 
 app.get('/app/', (req, res) => {
     res.statusCode = 200;
@@ -22,13 +15,11 @@ app.get('/app/', (req, res) => {
 
 app.get('/app/flip/', (req, res) => {
     res.statusCode = 200;
-    //res.writeHead(res.statusCode, {"Content-Type" : "text/plain"});
     res.send('{"flip":"' + coinFlip() + '"}');
 })
 
 app.get('/app/flips/:number/', (req, res) => {
     res.statusCode = 200;
-    //res.writeHead(res.statusCode, {"Content-Type" : "text/plain"});
     const flip_array = coinFlips(req.params.number);
     const sum = countFlips(flip_array)
     res.send('{"raw":[' + flip_array + '],"summary":{"tails":' + sum.get("tails") + ',"heads":' + sum.get("heads") + '}}')
@@ -36,14 +27,12 @@ app.get('/app/flips/:number/', (req, res) => {
 
 app.get('/app/flip/call/heads/', (req, res) => {
     res.statusCode = 200;
-    //res.writeHead(res.statusCode, {"Content-Type" : "text/plain"});
     const map = flipACoin("heads");
     res.send('{"call":"' + map.get("call") + '","flip":"' + map.get("flip") + '","result":"' + map.get("result") + '"}')
 })
 
 app.get('/app/flip/call/tails/', (req, res) => {
     res.statusCode = 200;
-    //res.writeHead(res.statusCode, {"Content-Type" : "text/plain"});
     const map = flipACoin("tails");
     res.send('{"call":"' + map.get("call") + '","flip":"' + map.get("flip") + '","result":"' + map.get("result") + '"}')
 })
@@ -51,12 +40,9 @@ app.get('/app/flip/call/tails/', (req, res) => {
 app.use(function(req, res){
     res.statusCode = 404;
     res.statusMessage = 'NOT FOUND'
-    //res.writeHead(res.statusCode, {"Content-Type" : "text/plain"});
     res.status(statusCode).end(statusCode + ' ' + statusMessage)
 });
 
 const server = app.listen(port, () => {
     console.log("App listening on port " + port)
 })
-
-// autograder is up woooo
